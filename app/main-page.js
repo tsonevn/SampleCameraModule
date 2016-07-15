d66c82451aad33d8c455fa87ac3da794294b6ba6
Nikolay Tsonev <nikolay.tsonev@telerik.com>
2016-07-15 16:26:03 +0300

    update project

--- app/main-page.js
+++ app/main-page.js
@@ -1,9 +1,6 @@
 "use strict";
 var main_view_model_1 = require("./main-view-model");
 var camera_1 = require("camera");
-var image_1 = require("ui/image");
-var prevImageSource;
-var num = 9;
 var img;
 // Event handler for Page "navigatingTo" event attached in main-page.xml
 function navigatingTo(args) {
@@ -17,16 +14,11 @@ function onTap(args) {
     //prevImageSource.android = null;
     camera_1.takePicture().then(function (picture) {
         console.log("Result is an image source instance");
-        var image = new image_1.Image();
-        image.imageSource = picture;
-        img.imageSource = image.imageSource;
-        num = num - 1;
-        prevImageSource = picture;
-        if (picture) {
-            picture.android.recycle();
-            picture.android = null;
-        }
-        console.log("counter " + num);
+        img.imageSource = picture;
+        // if(picture){
+        //     picture.android.recycle();
+        //     picture.android = null;
+        // }
     });
 }
 exports.onTap = onTap;
